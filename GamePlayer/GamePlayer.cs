5ff77676c732ed38fe2e3dad2f05f438df5efb34
Vadym Muntyan <vadym.muntyan@gmail.com>
2016-04-01 17:12:10 +0300

    initial commit

---
+++ GamePlayer/GamePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.IO.Pipes;
+
+namespace GamePlayer
+{
+    class GamePlayer
+    {
+        static void Main(string[] args)
+        {
+            var player = new NamedPipeClientStream(".", "The Game", PipeDirection.InOut);
+            StreamReader sr = new StreamReader(player);
+            StreamWriter sw = new StreamWriter(player);
+            player.Connect(100);
+
+            string handshake = sr.ReadLine();
+            if (handshake == "Waiting")
+            {
+                sw.WriteLine("Test Message");
+                sw.Flush();
+                player.Close();
+            }
+        }
+    }
+}
---
+++ GameServer/GameServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace GameServer
+{
+    class GameServer
+    {
+        static void Main(string[] args)
+        {
+            var server = new NamedPipeServerStream("The Game", PipeDirection.InOut);
+            bool isRunning = true;
+            StreamReader sr = new StreamReader(server);
+            StreamWriter sw = new StreamWriter(server);
+            do
+            {
+                Console.WriteLine("Waiting...");
+                server.WaitForConnection();
+                sw.WriteLine("Waiting");
+                sw.Flush();
+
+                server.WaitForPipeDrain();
+                string test = sr.ReadLine();
+                Console.WriteLine(test);
+                isRunning = false;
+            } while (isRunning);
+        }
+    }
+}
